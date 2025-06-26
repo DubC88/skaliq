1b2e25111743aefc83c304a1380d5ece89b6fb81
DubC88 <cnazon99@gmail.com>
2025-06-26 10:51:51 -0400

    google logo fix /favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,11 +28,23 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: '/favicon.ico', sizes: 'any' },
       { url: '/assets/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/assets/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/assets/favicon-48x48.png', sizes: '48x48', type: 'image/png' }
+    ],
+    apple: [
+      { url: '/assets/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ],
+    other: [
+      {
+        rel: 'mask-icon',
+        url: '/assets/safari-pinned-tab.svg',
+        color: '#5bbad5'
+      }
     ]
-  }
+  },
+  manifest: '/manifest.json'
 }
 
 export default function RootLayout({
